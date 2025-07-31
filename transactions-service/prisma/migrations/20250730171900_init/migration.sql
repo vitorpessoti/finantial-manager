ef3e084e665992f2b96f3b0f083c65a0b00ba356
Vitor Pessoti <vitorpessoti59@gmail.com>
2025-07-30 23:59:07 -0300

    Fixing migration file

--- transactions-service/prisma/migrations/20250730171900_init/migration.sql
+++ transactions-service/prisma/migrations/20250730171900_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `transaction` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "public"."transaction";
-
 -- CreateTable
 CREATE TABLE "public"."transactions" (
     "id" TEXT NOT NULL,
