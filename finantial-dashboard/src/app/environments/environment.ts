7744865744cb1ad17aa6deff16f829d3f6eae1b3
Vitor Pessoti <vitorpessoti59@gmail.com>
2026-06-24 15:23:19 -0300

    setting up docker

--- finantial-dashboard/src/app/environments/environment.ts
+++ finantial-dashboard/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     apiUrlLocal: 'http://localhost:3000/api/v1',
     apiUrl: 'https://finantial-manager.onrender.com/api/v1'
 }
